131399fdb719946a9a6dab53b41263e43b2a36f5
sammmmmm099 <srnvsxig@gmail.com>
2025-06-12 12:11:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,7 +85,7 @@ async def generate_string_session_command(event):
                 session_string = Prime.session.save()
                 me = await Prime.get_me()
                 text = f""
-👋 Hello {me.first_name},
+                👋 Hello {me.first_name},
 
 Here is your **Telethon String Session**:
 
